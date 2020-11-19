126d0fd74bb9d8e73dc6d3d3b24f66f361d7c623
kyleswillard <kyleswillard@gmail.com>
2020-11-19 08:23:37 -0800

    refactored original code, styled using styled components.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from "react";
-
-
 import Pic from './components/pic'
+import theme from './theme'
 import "./App.css";
+import { ThemeProvider } from "styled-components";
 
 function App() {
-
-
-
-
-
-
-
-
   return (
+
     <div className="App">
+      <ThemeProvider theme={theme}>
       <Pic />
+      </ThemeProvider>
     </div>
+
   );
 }
 
